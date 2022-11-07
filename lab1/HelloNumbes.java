f55463e43f5d44ce05af640d684cfb8b86e3d903
yxc <yxc2020@whu.edu.cn>
2022-11-07 22:08:13 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbes.java
@@ -0,0 +1,12 @@
+  public class HelloNumbers {
+	public static void main(String[] args) {
+		int x = 0;
+		while (x < 10) {
+//			System.out.println(x);
+			System.out.println(5 + "10");
+			x = x + 1;
+		}
+
+//		"x = "horse";"
+	}
+}
