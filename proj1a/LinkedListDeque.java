f78c64cb6ecf6428204cfd6242d926dda8b73d1e
XueXiaosong <yxc2020@foxmail.com>
2022-11-11 00:28:24 +0800

    LinkedListdeque functions init

---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,35 @@
+public class LinkedListDeque {
+    public void addFirst(T item){
+
+    }
+
+    public void addLast(T item){
+
+    }
+
+    public boolean isEmpty(){
+
+
+    }
+
+
+    public int size(){
+
+    }
+
+    public void printDeque(){
+
+    }
+
+    public T removeFirst(){
+
+    }
+
+    public T removeLast(){
+
+    }
+
+    public T get(int index){
+        
+    }
+}
